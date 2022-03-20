d97668acc24cc43157b17754d16eea0243bf59b6
Alejandro Cruz <hcruz0716@gmail.com>
2022-03-20 13:30:56 -0600

    First Commit

---
+++ components/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card>
+
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'LayoutTest',
+  layout: 'empty',
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <h1 class="text-3xl font-bold underline text-center">
+        Hello world!
+      </h1>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
